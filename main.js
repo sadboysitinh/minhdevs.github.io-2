1f79cb7893076041259d1dcdc2f3a70d8e3bf020
NGUYEN NGOC MINH <56338645+sadboysitinh@users.noreply.github.com>
2022-06-08 02:20:47 +0700

    Update main.js

--- main.js
+++ main.js
@@ -3,50 +3,48 @@ var meddiaPLayer = document.querySelector("audio");
 var playMusic = document.querySelector(".play-music");
 
 playMusic.onclick = () => {
-  meddiaPLayer.paused ? meddiaPLayer.play() : meddiaPLayer.pause();
+    meddiaPLayer.paused ? meddiaPLayer.play() : meddiaPLayer.pause();
 };
 // Animation progressbar
 var progressbar = document.querySelectorAll('[role="progressbar"]');
 
 const progressAnimation = (element) => {
-  let i = 0;
-  let WS = element.getAttribute("value");
-  let Animations;
-  Animations = setInterval(() => {
-    // element.innerText = i + "%";
-    element.style.width = i + "%";
-    i >= Number(WS) ? clearInterval(Animations) : i++;
-  }, 5);
+    let i = 0;
+    let WS = element.getAttribute("value");
+    let Animations;
+    Animations = setInterval(() => {
+        // element.innerText = i + "%";
+        element.style.width = i + "%";
+        i >= Number(WS) ? clearInterval(Animations) : i++;
+    }, 5);
 };
 
 for (var i = 0; i < progressbar.length; i++) {
-  progressAnimation(progressbar[i]);
+    progressAnimation(progressbar[i]);
 }
 
 // Coppy Payment information
-var items = document.querySelectorAll(
-  ".card.border-0.rounded-3.shadow.text-center.p-2.m-2"
-);
+var items = document.querySelectorAll(".card.border-0.rounded-3.shadow.text-center.p-2.m-2");
 
 itemsLength = items.length;
 
 for (var i = 0; i < itemsLength; i++) {
-  items[i].onclick = (e) => {
-    copyTextToClipboard(e);
-  };
+    items[i].onclick = (e) => {
+        copyTextToClipboard(e);
+    };
 }
 
 async function copyTextToClipboard(text) {
-  text = text.path[1].childNodes[4].innerText;
-  try {
-    await navigator.clipboard.writeText(text);
-    alert("Text copied to clipboard");
-  } catch (err) {}
+    text = text.path[1].childNodes[4].innerText;
+    try {
+        await navigator.clipboard.writeText(text);
+        alert("Text copied to clipboard");
+    } catch (err) {}
 }
 
 // Scroll page
 function scrollPage(height) {
-  window.scrollTo(0, height);
+    window.scrollTo(0, height);
 }
 
 // scrollPage(200);
